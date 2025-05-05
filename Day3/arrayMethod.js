dfb9b1bdfa7b9b7a4f70b05ab12733d1991b966d
dimka90 <dimkayilrit@gmail.com>
2025-05-05 22:20:42 +0100

    Added two more array methosd(shift and push) and also used process.argv for environment vairables

---
+++ Day3/arrayMethod.js
@@ -0,0 +1,16 @@
+
+export function pushElementToArray(array, element){
+    if (Array.isArray(array)){
+array.push(element)
+    }
+    else{
+        console.log("It is not an array")
+    }
+}
+
+
+
+export function  shiftArrayElement(array){
+    if(Array.isArray(array)){}
+    array.shift()
+}
--- Day3/modify_array.js
+++ Day3/modify_array.js
@@ -1,15 +1,54 @@
 // Refactor arrray1.js 
 // Declaring so many vairables
-let phones= ["Samsung","Infinix","Redmi","I-phone"]
 
+
+let phones = ["Samsung","Infinix","Redmi","I-phone"] // Array
+let handset = "Huawei" // String
+let num = 40 // Number
 userChoice = "Redmi"
 for(let i=0; i<phones.length; i++){
     if(phones[i] === userChoice){
         console.log(`${userChoice} is available`)
     }
 }
-console.log(process.argv[2])
 
-function pushElementToArray(element){
-    phones.push(element)
+// pushElementToArray(phones, "Nokia")
+
+// popArrayElement()
+
+// pushElementToArray(phones, "Nokia")
+// console.log(phones)
+
+
+// Deleting the last element from an array
+function  popArrayElement(array){
+    if(Array.isArray(array)){
+
+    
+    array.pop()
+    }
 }
+
+
+
+function pushElementToArray(array, element){
+    if (Array.isArray(array)){
+array.push(element)
+    }
+    else{
+        console.log("It is not an array")
+    }
+}
+
+
+
+ function  shiftArrayElement(array){
+    if(Array.isArray(array)){}
+    array.shift()
+}
+
+
+let userInput = process.argv[2]
+
+pushElementToArray(phones, userInput)
+console.log(phones)
